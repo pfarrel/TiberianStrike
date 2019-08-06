d1d5a81ec827de9509dbf258c213fdfba1dd5e87
Padraig Farrell <pfarrel@tcd.ie>
2019-08-06 21:14:57 +0100

    Remove game file comments

--- CncTd/CncTdGame.cs
+++ CncTd/CncTdGame.cs
@@ -8,9 +8,6 @@ using System.Linq;
 
 namespace CncTd
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class CncTdGame : Game
     {
         GraphicsDeviceManager graphics;
@@ -36,12 +33,6 @@ namespace CncTd
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             IsMouseVisible = true;
@@ -64,10 +55,6 @@ namespace CncTd
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
@@ -78,20 +65,11 @@ namespace CncTd
             Fonts.Load(Content);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -235,10 +213,6 @@ namespace CncTd
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
